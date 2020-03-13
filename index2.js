a635cf61fa8e1c0bd983a78e629dba17cd244a1d
Adrian Faustino <contact.adrianfaustino@gmail.com>
2020-03-13 16:46:53 +0000

    First commit

--- index2.js
+++ index2.js
@@ -15,10 +15,3 @@ function newTask(title, description) {
 
   return task;
 };
-
-// create new tasks by storing them to a variable and calling the new task constructor
-const task1 = newTask('Clear room', 'Pick up all the clothes on the floor.');
-const task2 = newTask(`Compass`, `Finish all of today's modules.`);
-task1.logState();
-task2.markCompleted();
-task2.logState();
